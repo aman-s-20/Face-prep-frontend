a52df2810051ee1723954a3135336933d0a4aeae
Aman Singh <singhaman235136@gmail.com>
2023-03-31 07:40:36 +0530

    First commit5

--- src/App.js
+++ src/App.js
@@ -3,43 +3,43 @@ import './App.css';
 import Home from './components/Home'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import Signup from './components/Signup';
 import Login from './components/Login';
 // eslint-disable-next-line
-import { useState ,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [auth,setAuth] = useState(localStorage.getItem("user"));
-   
+  const [auth, setAuth] = useState(localStorage.getItem("user"));
+
 
   const [userDetail, setUserDetail] = useState([]);
   const addUser = (newUser) => {
     setUserDetail((prevUser) => {
-      return [...prevUser,newUser];
+      return [...prevUser, newUser];
     })
   }
-  
- 
-  const checkUser= (currentUser)=>{
 
-    const result = userDetail.find((user)=>
-     user.username === currentUser.username);
 
-    if(result && result.password===currentUser.password){
-       console.log(auth);
+  const checkUser = (currentUser) => {
+
+    const result = userDetail.find((user) =>
+      user.username === currentUser.username);
+
+    if (result && result.password === currentUser.password) {
+      console.log(auth);
       console.log("Logged in successfully");
-       setAuth(localStorage.setItem('user',JSON.stringify(result)));
+      setAuth(localStorage.setItem('user', JSON.stringify(result)));
     }
-    else{
-       alert("Invaid username or password.");
+    else {
+      alert("Invaid username or password.");
     }
 
   }
 
 
-  
+
 
   return (
     <div className="App">
@@ -51,8 +51,12 @@ function App() {
           <Route element={<PrivateRoute />}>
             <Route path='/home' element={<Home />} />
           </Route>
-          <Route path='/login' element={<Login getUser={checkUser}/>} />
+          <Route path='/login' element={<Login getUser={checkUser} />} />
           <Route path='/signup' element={<Signup onAdd={addUser} />} />
+          <Route
+            path="*"
+            element={<Navigate to="/signup" />}
+          />
         </Routes>
       </BrowserRouter>
       <Footer />
